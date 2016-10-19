82a80ca8942bdbda0f8ec9b5b093d6efe2d532c6
lyj2576 <295441510@qq.com>
2016-10-19 21:31:04 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, this project is used to learn algorithms and data structures.");
+            Console.ReadKey();
+        }
+    }
+}
